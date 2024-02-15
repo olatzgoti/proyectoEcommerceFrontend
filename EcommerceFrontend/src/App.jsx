a094320b0acd903a063b83d1252712b94f709311
IKLANLO <ilandaberea@yahoo.es>
2024-02-15 20:03:28 +0100

    añadido nav con ant design

--- EcommerceFrontend/src/App.jsx
+++ EcommerceFrontend/src/App.jsx
@@ -1,18 +1,25 @@
-import './App.css'
+import './App.scss'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
+import Products from './components/Products/Products'
+
 // import { GlobalProvider } from './context/GlobalState'
 
 function App() {
   return (
     // <GlobalProvider>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-        </Routes>
-        {/* <Footer/> */}
+        <div className='main-container'>
+          <Header />
+          <div className='main-container-options'>
+            <Routes>
+              <Route path='/' element={<Home/>}/>
+              <Route path='/products' element={<Products/>}/>
+            </Routes>
+          </div>
+          {/* <Footer/> */}
+        </div>
       </BrowserRouter>
     // </GlobalProvider>
   )
--- EcommerceFrontend/src/components/Header/Header.jsx
+++ EcommerceFrontend/src/components/Header/Header.jsx
@@ -1,17 +1,73 @@
-import { Link } from 'react-router-dom'
-// import './Header.styles.scss'
+import {  useNavigate } from 'react-router-dom'
+import { React, useState } from 'react';
+import { AndroidOutlined, AuditOutlined, LoginOutlined, AppstoreOutlined, MailOutlined } from '@ant-design/icons';
+import { Menu } from 'antd';
+
+// function getItem(label, key, icon, children, type) {
+//   return {
+//     key,
+//     icon,
+//     children,
+//     label,
+//     type,
+//   };
+// }
+
+const items = [
+  {
+    label: 'Home',
+    key: 'home',
+    icon: <MailOutlined />,
+  },
+  {
+    label: 'Login',
+    key: 'login',
+    icon: <LoginOutlined />,
+    // disabled: true,
+  },
+  {
+    label: 'Registro',
+    key: 'registro',
+    icon: <AuditOutlined />,
+  },
+  {
+    label: 'Productos',
+    key: 'productos',
+    icon: <AppstoreOutlined />,
+  },
+  {
+    label: 'Perfil',
+    key: 'perfil',
+    icon: <AndroidOutlined />,
+  },
+]
 
 const Header = () => {
-  return (
-    <nav className='header'>
-      <div>
-        <span><Link className='header__link' to='/'>Home</Link></span>
-        <span><Link className='header__link' to='/login'>Login</Link></span>
-        <span><Link className='header__link' to='/register'>Registro</Link></span>
-        <span><Link className='header__link' to='/profile'>Perfil</Link></span>
-      </div>
-    </nav>
-  )
+  let navigate = useNavigate()
+  const [current, setCurrent] = useState('home')
+
+  const onClick = (e) => {
+    setCurrent(e.key)
+    switch (e.key){ 
+          case 'home': 
+            navigate('/')
+            break
+          case 'login': 
+            navigate('/login')
+            break
+          case 'registro': 
+            navigate('/register')
+            break
+          case 'productos': 
+            navigate('/products')
+            break
+          case 'perfil': 
+            navigate('/profile')
+            break
+        }
+  }
+
+  return <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />
 }
 
 export default Header
