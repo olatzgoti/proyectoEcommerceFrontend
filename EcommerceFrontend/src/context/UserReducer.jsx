cb10c23d7e43fa47e53e7c41058996ad859c83cf
olatz <gotiolatz@gmail.com>
2024-02-19 19:54:56 +0100

    userReducer

---
+++ EcommerceFrontend/src/context/UserReducer.jsx
@@ -0,0 +1,27 @@
+const users = (state, action)=> {
+
+    switch (action.type) {
+        case 'REGISTER':
+            return {
+                ...state,
+                user:action.payload,
+
+            }
+        case 'LOGIN':
+            return {
+                ...state,
+                token: action.payload.token,
+
+            }
+        case 'LOGOUT':
+            return {
+                user:null,
+                token:null,
+            
+            }    
+        default:
+            return state
+    }
+}
+
+export default users
