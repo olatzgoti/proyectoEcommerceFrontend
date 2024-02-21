9a2a86aae57838e859d34242f5146bc2ac48f7f2
olatz <gotiolatz@gmail.com>
2024-02-21 20:08:00 +0100

    descartes

---
+++ EcommerceFrontend/src/components/Cart/Cart.jsx
@@ -0,0 +1,32 @@
+import { useContext } from 'react'
+import { ProductContext } from "../../context/ProductState"
+
+
+const Cart = () => {
+    const { cart } = useContext(ProductContext);
+    if (cart.length <= 0) {
+    return <span>No tienes ningún producto en el carrito</span>;
+    }
+    useEffect(() => {
+localStorage.setItem("cart", JSON.stringify(cart));
+}, [cart]);
+
+    const cartItem = cart.map((cartItem, i) => {
+    return (
+
+    <div key={i}>
+        <span>{cartItem.name}</span>
+       <span>{cartItem.price.toFixed(2)} €</span>
+    </div>
+    )
+    });
+
+    return (
+    <div>
+        {cartItem}
+        <button onClick={() => clearCart()}>Clear cart</button>
+    </div>
+    );
+    };
+
+export default Cart;
