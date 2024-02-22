06fb70a48cb08cf4c8fb1c2a03c4e03a2689eba9
olatz <gotiolatz@gmail.com>
2024-02-22 15:14:24 +0100

    Formato-login

--- EcommerceFrontend/src/components/Login/Login.jsx
+++ EcommerceFrontend/src/components/Login/Login.jsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { UserDataContext } from '../../context/UserState'
-import { Form, Input, Button } from 'antd'
+import { Form, Input, Button, Space } from 'antd'
 
 const Login = () => {
 
@@ -20,20 +20,25 @@ const Login = () => {
 
     return(
     <>  
-        <Form onFinish={onFinish} initialValues={{ remember:true }} onFinishFailed={onFinishFailed}autocomplete="off">
-            <Form.Item label="Email" name="email" rules={[{ required: true, message: 'Please, insert your email' }]}>
-                <Input/>
+     <Space className='space' align='center' direction='vertical' style={{'padding-top': '1.25rem'}}> 
+        <Form name="basic" onFinish={onFinish} initialValues={{ remember:true }} onFinishFailed={onFinishFailed} autocomplete="off">
+        
+            <Form.Item name="email" rules={[{ required: true, message: 'Please, insert your email' }]}>
+                <Input placeholder='Email'/>
             </Form.Item>
-            <Form.Item label="Password" name="password" rules={[{ required: true, message: 'Please, insert your password' }]}>
-                <Input.Password />
+
+            <Form.Item name="password" rules={[{ required: true, message: 'Please, insert your password' }]}>
+                
+                <Input.Password placeholder='Password'/>
             </Form.Item>
 
             <Form.Item>
-                <Button htmlType="submit">
+                <Button type="primary" htmlType="submit">
                     Submit
                 </Button>
             </Form.Item>
         </Form>
+      </Space>
     </>
     )
     }    
