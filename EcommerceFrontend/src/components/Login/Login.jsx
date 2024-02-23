e83b201c265ba344a50424904b219c16f2c8dd50
olatz <gotiolatz@gmail.com>
2024-02-23 18:41:42 +0100

    login y avance carrito

--- EcommerceFrontend/src/components/Login/Login.jsx
+++ EcommerceFrontend/src/components/Login/Login.jsx
@@ -1,39 +1,33 @@
-import { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { UserDataContext } from '../../context/UserState'
 import { Form, Input, Button, Space, Alert } from 'antd'
 
 const Login = () => {
-
     const { login } = useContext(UserDataContext) 
     const [ showAlert, setShowAlert ] = useState(false)
-    
     const [ alertMessage, setAlertMessage ] = useState('')
-
+    let navigate = useNavigate()
+    
     useEffect(() => {
         setTimeout(()=>{
             const foundToken = JSON.parse(localStorage.getItem('token'))
-            if(foundToken)
-            { setShowAlert(true)
-                setAlertMessage('Se ha efectuado el inicio de sesión correctamente')
+            if(foundToken){
+   //         
+                //setShowAlert(true)
+     //           setAlertMessage('Se ha efectuado el inicio de sesión correctamente')
                 navigate('/users/getUser') }}, 1500)},
                 [login])      
-                /*
-                useEffect(() => {
-                    if (users === 'email already used') {
-                        setShowAlert(true)
-                        setAlertMessage('Email ya utilizado, escoge otro')
-                    }
-                }, [users])
-                */
+console.log('hola1')
 
-    let navigate = useNavigate()
     const onFinish = async(values) => 
     { console.log('Success', values)
+    console.log('hola2')
         try {
-        await login(values)
-        setShowAlert(true)
-        setAlertMessage('Ha iniciado sesión correctamente')  
+            console.log('hola3')
+            await login(values)
+            setShowAlert(true)
+            setAlertMessage('Ha iniciado sesión correctamente')  
         } catch (error) {
             console.log(error)
         }
@@ -41,14 +35,12 @@ const Login = () => {
         
     const onFinishFailed = (error) => { console.log("Failed:", error)}
 
-    const handleAlert = () => {
-        setShowAlert(false)
-    }
+    const handleAlert = () => { setShowAlert(false) }
 
     const alert = <Alert message={alertMessage} type="info" showIcon closable onClose={handleAlert}/>
-
+    
     return(
-    <>  
+        <>  
     <Space className='space' align='center' direction='vertical' style={{'padding-top': '1.25rem'}}> 
         <Form name="basic" onFinish={onFinish} initialValues={{ remember:true }} onFinishFailed={onFinishFailed} autocomplete="off">
         
@@ -62,9 +54,7 @@ const Login = () => {
             </Form.Item>
 
             <Form.Item>
-                <Button type="primary" htmlType="submit">
-                    Submit
-                </Button>
+                <Button type="primary" htmlType="submit"> Enviar </Button>
             </Form.Item>
         </Form>
     </Space>
